596336294dfed0ea23d37c712538f9e6e69cd7b0
Armin <Armin.Mahdilou@gmail.com>
2023-12-01 22:05:41 +0100

    Conf changed

--- system/vm.nix
+++ system/vm.nix
@@ -1,4 +1,13 @@
 { lib, ... }: {
+  networking = {
+    hostName = lib.mkForce "vmnix";
+    firewall = {
+      allowPing = lib.mkForce false;
+      allowedTCPPorts = lib.mkForce [ 22 ];
+      allowedUDPPorts = lib.mkForce [  ];
+      extraPackages = lib.mkForce [ pkgs.conntrack-tools ];
+    };
+  };
   services = {
     k3s.enable = lib.mkForce true;
     qemuGuest.enable = true;
