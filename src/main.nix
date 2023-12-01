237699d64d26791188c5ca75cadc01f03c716a2f
Armin <Armin.Mahdilou@gmail.com>
2023-12-01 16:39:51 +0100

    Conf changed

--- src/main.nix
+++ src/main.nix
@@ -1,5 +1,5 @@
 { configRepo, config, pkgs, lib, ... }:
-#let homeManager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz"; in
+let homeManager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz"; in
 let customConfs = pkgs.callPackage (import "${configRepo}/src/confs.nix") {}; in
 let customPkgs = pkgs.callPackage (import "${configRepo}/src/pkgs.nix") {}; in
 {
