ee420d9aaca34a1a06b45e493d2a355597ef2eb8
Armin <Armin.Mahdilou@gmail.com>
2023-12-01 21:37:57 +0100

    Conf changed

--- README.md
+++
@@ -1,7 +0,0 @@
-# 1nv0k32 NixOS configs
-
-``` shell
-$ sudo cp -r nixos /etc/nixos
-$ sudo nixos-rebuild switch --upgrade
-```
-
--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,11 @@
-{ lib, ... }:
+{ ... }:
 let configRepo = builtins.fetchGit { url = "https://github.com/1nv0k32/NixOS.git"; ref = "main"; }; in
 {
   imports = [
     ./hardware-configuration.nix
     (import "${configRepo}/src/main.nix")
-  ] ++
-  lib.optional (builtins.pathExists ./system.nix) ./system.nix;
+    (import "${configRepo}/system/z13.nix")
+  ];
 }
 
 # vim:expandtab ts=2 sw=2
