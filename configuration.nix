60e3d02e580d3debf8d21d7bec89b3733e4456fe
Armin <Armin.Mahdilou@gmail.com>
2023-12-05 13:42:15 +0100

    Conf changed

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,14 @@
-{ ... }:
+{ lib, ... }:
 let configRepo = builtins.fetchGit { url = "https://github.com/1nv0k32/NixOS.git"; ref = "main"; }; in
 {
-  imports = [
-    ./hardware-configuration.nix
-    (import "${configRepo}/src")
-    (import "${configRepo}/system/z13.nix")
-    #(import "${configRepo}/system/usb.nix")
-    #(import "${configRepo}/system/vm.nix")
-  ];
+  imports = []
+  ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) ./hardware-configuration.nix
+  ++ [
+      (import "${configRepo}/src")
+      (import "${configRepo}/system/z13.nix")
+      #(import "${configRepo}/system/usb.nix")
+      #(import "${configRepo}/system/vm.nix")
+    ];
 }
 
 # vim:expandtab ts=2 sw=2
