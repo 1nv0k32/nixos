a45f5e9c5700ddf5a4664ee17bf11d2f7878e8d6
Armin <Armin.Mahdilou@gmail.com>
2023-12-06 15:24:49 +0100

    Conf changed

--- system/wsl.nix
+++ system/wsl.nix
@@ -15,10 +15,34 @@
     wsl-vpnkit
   ];
 
+  systemd.services."wsl-vpnkit" = {
+    enable = true;
+    description = "wsl-vpnkit service"
+    unitConfig = {
+      After = "network.target";
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.wsl-vpnkit}/bin/wsl-vpnkit";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  services.resolved.extraConfig = lib.mkForce ''
+    [Resolve]
+    DNS=8.8.8.8
+    #Domains=
+    DNSSEC=no
+    #DNSOverTLS=no
+    MulticastDNS=no
+    LLMNR=no
+    Cache=no
+    CacheFromLocalhost=no
+    DNSStubListener=no
+  '';
+
   boot.loader.systemd-boot.enable = lib.mkForce false;
   networking.networkmanager.enable = lib.mkForce false;
   networking.firewall.enable = lib.mkForce false;
-  services.resolved.enable = lib.mkForce false;
 }
 
 # vim:expandtab ts=2 sw=2
