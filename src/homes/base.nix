a027a7e6dfc78d93a440a06f3e662a95a5c07dd3
Armin <Armin.Mahdilou@gmail.com>
2023-12-22 16:49:52 +0100

    Conf changed

--- src/homes/base.nix
+++ src/homes/base.nix
@@ -61,11 +61,11 @@ with lib.hm.gvariant;
     "org/gnome/desktop/session" = {
       idle-delay = mkUint32 0;
     };
-    "org/gnome/desktop/background" = {
-      picture-options = "none";
-      primary-color = "#000000";
-      secondary-color = "#000000";
-    };
+    # "org/gnome/desktop/background" = {
+    #   picture-options = "none";
+    #   primary-color = "#000000";
+    #   secondary-color = "#000000";
+    # };
     "org/gnome/desktop/notifications" = {
       show-in-lock-screen = false;
     };
