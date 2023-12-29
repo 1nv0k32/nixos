c10ffe2036e573b094a17d5d203600e9b6f242b0
Armin <Armin.Mahdilou@gmail.com>
2023-12-29 14:59:46 +0100

    2023/12/29-14:59:46

--- system/z13.nix
+++ system/z13.nix
@@ -3,7 +3,7 @@ let luksDevs = config.boot.initrd.luks.devices; in
 with lib;
 {
   boot.kernelParams = options.boot.kernelParams.default ++ [ "amd_pstate=passive" ];
-  boot.initrd.luks.devices = mkIf (boot.initrd.luks.devices ? "root") { "root".crypttabExtraOpts = [ "tpm2-device=auto" ]; };
+  boot.initrd.luks.devices = mkIf (config.boot.initrd.luks.devices ? "root") { "root".crypttabExtraOpts = [ "tpm2-device=auto" ]; };
 
   services = {
     fprintd.enable = true;
