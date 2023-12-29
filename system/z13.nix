f991bf2359ee2bf507e827a35af7eaf69a045867
Armin <Armin.Mahdilou@gmail.com>
2023-12-29 14:35:40 +0100

    2023/12/29-14:35:40

--- system/z13.nix
+++ system/z13.nix
@@ -2,7 +2,7 @@
 with lib;
 {
   boot.kernelParams = options.boot.kernelParams.default ++ [ "amd_pstate=passive" ];
-  boot.initrd.luks.devices = mkIf (config.boot.initrd.luks != {}) {
+  boot.initrd.luks.devices = mkIf (config.boot.initrd.luks.devices != {}) {
     "root" = {
     #   device = config.boot.initrd.luks.devices."root".device;
       crypttabExtraOpts = [ "tpm2-device=auto" ];
